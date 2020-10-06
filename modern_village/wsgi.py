c6e11e678437773985472fbc1e0a2b582ddbb47f
Nikal Morgan <nikalmorgan11@gmail.com>
2020-10-06 10:09:06 -0500

    Set up django project and apps

---
+++ modern_village/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for modern_village project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modern_village.settings')
+
+application = get_wsgi_application()
