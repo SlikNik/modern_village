a6d7d902d28529c8179529cce489c2185ef8a6c1
Jo Anna Mollman <joanna.mollman@gmail.com>
2020-10-08 11:53:27 -0700

    add static file

--- modern_village/settings.py
+++ modern_village/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +128,7 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'modern_users.ModernUsers'
 
 LOGIN_URL = '/login/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
