f5401ee6a8902d66d59aeef7d59dbe24de3496c9
Jo Anna Mollman <joanna.mollman@gmail.com>
2020-10-19 12:18:23 -0700

    Revert "fix secret key"
    This reverts commit 7a2eddb3be48f06a754c537b322c7eea46ec5a83.
    git commit -m "reverting to earlier branch has 7a2eddb

--- modern_village/settings.py
+++ modern_village/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv("secrets.env")
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
