6be52bbe02697b9e8d9be923c74ebe77d3c901f2
Jo Anna Mollman <joanna.mollman@gmail.com>
2020-10-19 16:12:27 -0700

    last changes to fix issues

--- modern_village/settings.py
+++ modern_village/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv("secrets.env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_key = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
- 
+
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
