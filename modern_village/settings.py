7a2eddb3be48f06a754c537b322c7eea46ec5a83
Jo Anna Mollman <joanna.mollman@gmail.com>
2020-10-19 10:53:14 -0700

    fix secret key

--- modern_village/settings.py
+++ modern_village/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv("secrets.env")
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
